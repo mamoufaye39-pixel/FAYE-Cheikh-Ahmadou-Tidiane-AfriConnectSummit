2af166327e94470f9029d6f982ae573d8428653b
mamoufaye39-pixel <mamoufaye39@gmail.com>
2026-07-27 14:49:07 +0000

    Création de la section Hero de la page d'accueil

--- FAYE-Cheikh-Ahmadou-Tidiane-AfriConnectSummit/js/main.js
+++ FAYE-Cheikh-Ahmadou-Tidiane-AfriConnectSummit/js/main.js
@@ -2,3 +2,9 @@
 // AfriConnect Summit 2026
 // JavaScript principal
 // ===============================
+
+
+// ===============================
+// Compte à rebours
+// (sera développé au prochain commit)
+// ===============================
